a860a469a3f66bd5a5f4a12889d3b0f9c5bbd456
Salomon Beza <beza.salomon@gmail.com>
2017-01-17 15:45:02 +0200

    completed the exercise by creating the lottery and lyrics pages

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -6,4 +6,23 @@ class PagesController < ApplicationController
     render "fortune_teller.html"
   end
 
+  def lottery_numbers
+  	@array= []
+  	10.times do
+  	@array << rand(100)
+  	end
+  	puts @array
+  	render "lottery_numbers.html"
+
+  end
+
+  def lyrics
+  	@lyric = ""
+  	count = 99
+  	99.times do
+  	@lyric << " #{count} bottles of beer on the wall, #{count} bottles of beer. Take one down and pass it around, #{count-1} bottles of beer on the wall. #{count-1} bottles of beer on the wall,"
+  	count -= 1
+  	end
+  end
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get "/fortune_teller", to: 'pages#fortune_teller'
+get "lottery_numbers", to: 'pages#lottery_numbers'
+get "lyrics", to: 'pages#lyrics'
 end
